tsshadow/ultrasonic

commit 725d9281bfcfe43898535f3a19e252a4f4c1c970

Move search button in to main Topbar menu

ultrasonic/src/main/kotlin/org/moire/ultrasonic/fragment/TrackCollectionFragment.kt
@@ -36,7 +36,6 @@ import org.moire.ultrasonic.adapters.AlbumHeader
 import org.moire.ultrasonic.adapters.AlbumRowDelegate
 import org.moire.ultrasonic.adapters.HeaderViewBinder
 import org.moire.ultrasonic.adapters.TrackViewBinder
-import org.moire.ultrasonic.data.ActiveServerProvider
 import org.moire.ultrasonic.data.ActiveServerProvider.Companion.isOffline
 import org.moire.ultrasonic.domain.Identifiable
 import org.moire.ultrasonic.domain.MusicDirectory
@@ -64,7 +63,6 @@ import timber.log.Timber
  * In most cases the data should be just a list of Entries, but there are some cases
  * where the list can contain Albums as well. This happens especially when having ID3 tags disabled,
  * or using Offline mode, both in which Indexes instead of Artists are being used.
- *
  */
 @Suppress("TooManyFunctions")
 open class TrackCollectionFragment(
@@ -268,6 +266,9 @@ open class TrackCollectionFragment(
 
     private val menuProvider: MenuProvider = object : MenuProvider {
         override fun onPrepareMenu(menu: Menu) {
+            // Hide search button (from xml)
+            menu.findItem(R.id.action_search).isVisible = false
+
             playAllButton = menu.findItem(R.id.select_album_play_all)
 
             if (playAllButton != null) {
@@ -282,7 +283,7 @@ open class TrackCollectionFragment(
         }
 
         override fun onCreateMenu(menu: Menu, inflater: MenuInflater) {
-            inflater.inflate(R.menu.select_album, menu)
+            inflater.inflate(R.menu.track_collection_menu, menu)
         }
 
         override fun onMenuItemSelected(item: MenuItem): Boolean {
@@ -584,12 +585,8 @@ open class TrackCollectionFragment(
             } else {
                 setTitle(name)
 
-                if (ActiveServerProvider.shouldUseId3Tags()) {
-                    if (isAlbum) {
-                        listModel.getAlbum(refresh2, id, name)
-                    } else {
-                        throw IllegalAccessException("Use AlbumFragment instead!")
-                    }
+                if (isAlbum) {
+                    listModel.getAlbum(refresh2, id, name)
                 } else {
                     listModel.getMusicDirectory(refresh2, id, name)
                 }

gradle/libs.versions.toml
@@ -2,7 +2,7 @@
 # You need to run ./gradlew wrapper after updating the version
 gradle                 = "8.1.1"
 
-navigation             = "2.5.3"
+navigation             = "2.6.0"
 gradlePlugin           = "8.0.2"
 androidxcore           = "1.10.1"
 ktlint                 = "0.43.2"

ultrasonic/src/main/java/org/moire/ultrasonic/provider/SearchSuggestionProvider.java
@@ -1,37 +1,0 @@
-/*
- This file is part of Subsonic.
-
- Subsonic is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- Subsonic is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with Subsonic.  If not, see <http://www.gnu.org/licenses/>.
-
- Copyright 2010 (C) Sindre Mehus
- */
-package org.moire.ultrasonic.provider;
-
-import android.content.SearchRecentSuggestionsProvider;
-
-/**
- * Provides search suggestions based on recent searches.
- *
- * @author Sindre Mehus
- */
-public class SearchSuggestionProvider extends SearchRecentSuggestionsProvider
-{
-	public static final String AUTHORITY = SearchSuggestionProvider.class.getName();
-	public static final int MODE = DATABASE_MODE_QUERIES;
-
-	public SearchSuggestionProvider()
-	{
-		setupSuggestions(AUTHORITY, MODE);
-	}
-}

ultrasonic/src/main/kotlin/org/moire/ultrasonic/activity/NavigationActivity.kt
@@ -18,16 +18,20 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.provider.SearchRecentSuggestions
 import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
+import androidx.core.view.MenuProvider
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.FragmentContainerView
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player.STATE_BUFFERING
@@ -93,6 +97,10 @@ class NavigationActivity : AppCompatActivity() {
     private var host: NavHostFragment? = null
     private var selectServerButton: MaterialButton? = null
     private var headerBackgroundImage: ImageView? = null
+
+    // We store the last search string in this variable.
+    // Seems a bit like a hack, is there a better way?
+    var searchQuery: String? = null
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
@@ -221,10 +229,57 @@ class NavigationActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1 && !UApp.instance!!.isFirstRun) {
             ShortcutUtil.registerShortcuts(this)
         }
+
+        // Register our options menu
+        addMenuProvider(
+            searchMenuProvider,
+            this,
+            Lifecycle.State.RESUMED
+        )
+    }
+
+    private val searchMenuProvider: MenuProvider = object : MenuProvider {
+        override fun onPrepareMenu(menu: Menu) {
+            setupSearchField(menu)
+        }
+
+        override fun onCreateMenu(menu: Menu, inflater: MenuInflater) {
+            inflater.inflate(R.menu.search_view_menu, menu)
+        }
+
+        override fun onMenuItemSelected(item: MenuItem): Boolean {
+            return false
+        }
+    }
+
+    fun setupSearchField(menu: Menu) {
+        Timber.i("Recreating search field")
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+        val searchableInfo = searchManager.getSearchableInfo(this.componentName)
+        searchView.setSearchableInfo(searchableInfo)
+        searchView.setIconifiedByDefault(false)
+
+        if (searchQuery != null) {
+            Timber.e("Found existing search query")
+            searchItem.expandActionView()
+            searchView.isIconified = false
+            searchView.setQuery(searchQuery, false)
+            searchView.clearFocus()
+            // Restore search text only once!
+            searchQuery = null
+        }
     }
 
     private fun setupDrawerLayout(drawerLayout: DrawerLayout) {
+        // Set initial state passed on drawer state
+        closeNavigationDrawerOnBack.isEnabled = drawerLayout.isOpen
+
+        // Add the back press listener
         onBackPressedDispatcher.addCallback(this, closeNavigationDrawerOnBack)
+
+        // Listen to changes in the drawer state and enable the back press listener accordingly.
         drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener {
             override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
                 // Nothing
@@ -392,47 +447,55 @@ class NavigationActivity : AppCompatActivity() {
         return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
     }
 
-    // TODO: Test if this works with external Intents
-    // android.intent.action.SEARCH and android.media.action.MEDIA_PLAY_FROM_SEARCH calls here
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
-        if (intent == null) return
 
-        if (intent.action == Constants.INTENT_PLAY_RANDOM_SONGS) {
-            val currentFragment = host?.childFragmentManager?.fragments?.last() ?: return
-            val service = MusicServiceFactory.getMusicService()
-            val musicDirectory = service.getRandomSongs(Settings.maxSongs)
-            val downloadHandler: DownloadHandler by inject()
-            downloadHandler.addTracksToMediaController(
-                songs = musicDirectory.getTracks(),
-                append = false,
-                playNext = false,
-                autoPlay = true,
-                shuffle = false,
-                fragment = currentFragment,
-                playlistName = null
-            )
-            return
+        when (intent?.action) {
+            Constants.INTENT_PLAY_RANDOM_SONGS -> {
+                playRandomSongs()
+            }
+            Intent.ACTION_MAIN -> {
+                if (intent.getBooleanExtra(Constants.INTENT_SHOW_PLAYER, false)) {
+                    findNavController(R.id.nav_host_fragment).navigate(R.id.playerFragment)
+                }
+            }
+            Intent.ACTION_SEARCH -> {
+                searchQuery = intent.getStringExtra(SearchManager.QUERY)
+                handleSearchIntent(searchQuery, false)
+            }
+            MediaStore.INTENT_ACTION_MEDIA_PLAY_FROM_SEARCH -> {
+                searchQuery = intent.getStringExtra(SearchManager.QUERY)
+                handleSearchIntent(searchQuery, true)
+            }
         }
+    }
 
-        if (intent.getBooleanExtra(Constants.INTENT_SHOW_PLAYER, false)) {
-            findNavController(R.id.nav_host_fragment).navigate(R.id.playerFragment)
-            return
-        }
+    private fun handleSearchIntent(query: String?, autoPlay: Boolean) {
+        val suggestions = SearchRecentSuggestions(
+            this,
+            SearchSuggestionProvider.AUTHORITY, SearchSuggestionProvider.MODE
+        )
+        suggestions.saveRecentQuery(query, null)
 
-        val query = intent.getStringExtra(SearchManager.QUERY)
+        val action = NavigationGraphDirections.toSearchFragment(query, autoPlay)
+        findNavController(R.id.nav_host_fragment).navigate(action)
+    }
 
-        if (query != null) {
-            val autoPlay = intent.action == MediaStore.INTENT_ACTION_MEDIA_PLAY_FROM_SEARCH
-            val suggestions = SearchRecentSuggestions(
-                this,
-                SearchSuggestionProvider.AUTHORITY, SearchSuggestionProvider.MODE
-            )
-            suggestions.saveRecentQuery(query, null)
-
-            val action = NavigationGraphDirections.toSearchFragment(query, autoPlay)
-            findNavController(R.id.nav_host_fragment).navigate(action)
-        }
+    private fun playRandomSongs() {
+        val currentFragment = host?.childFragmentManager?.fragments?.last() ?: return
+        val service = MusicServiceFactory.getMusicService()
+        val musicDirectory = service.getRandomSongs(Settings.maxSongs)
+        val downloadHandler: DownloadHandler by inject()
+        downloadHandler.addTracksToMediaController(
+            songs = musicDirectory.getTracks(),
+            append = false,
+            playNext = false,
+            autoPlay = true,
+            shuffle = false,
+            fragment = currentFragment,
+            playlistName = null
+        )
+        return
     }
 
     /**

ultrasonic/src/main/kotlin/org/moire/ultrasonic/fragment/SearchFragment.kt
@@ -7,19 +7,11 @@
 
 package org.moire.ultrasonic.fragment
 
-import android.app.SearchManager
-import android.content.Context
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
-import androidx.appcompat.widget.SearchView
-import androidx.core.view.MenuHost
-import androidx.core.view.MenuProvider
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -54,16 +46,14 @@ import org.moire.ultrasonic.util.CommunicationError
 import org.moire.ultrasonic.util.Settings
 import org.moire.ultrasonic.util.Util
 import org.moire.ultrasonic.util.Util.toast
-import timber.log.Timber
 
 /**
  * Initiates a search on the media library and displays the results
- * TODO: Switch to material3 class
+
  */
 class SearchFragment : MultiListFragment<Identifiable>(), KoinComponent {
     private var searchResult: SearchResult? = null
     private var searchRefresh: SwipeRefreshLayout? = null
-    private var searchView: SearchView? = null
 
     private val mediaPlayerManager: MediaPlayerManager by inject()
 
@@ -82,13 +72,6 @@ class SearchFragment : MultiListFragment<Identifiable>(), KoinComponent {
         super.onViewCreated(view, savedInstanceState)
         cancellationToken = CancellationToken()
         setTitle(this, R.string.search_title)
-
-        // Register our options menu
-        (requireActivity() as MenuHost).addMenuProvider(
-            menuProvider,
-            viewLifecycleOwner,
-            Lifecycle.State.RESUMED
-        )
 
         listModel.searchResult.observe(
             viewLifecycleOwner
@@ -146,73 +129,6 @@ class SearchFragment : MultiListFragment<Identifiable>(), KoinComponent {
         if (navArgs.query != null) {
             return search(navArgs.query!!, navArgs.autoplay)
         }
-    }
-
-    /**
-     * This provide creates the search bar above the recycler view
-     */
-    private val menuProvider: MenuProvider = object : MenuProvider {
-        override fun onPrepareMenu(menu: Menu) {
-            setupOptionsMenu(menu)
-        }
-
-        override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-            menuInflater.inflate(R.menu.search, menu)
-        }
-
-        override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-            return true
-        }
-    }
-    fun setupOptionsMenu(menu: Menu) {
-        val activity = activity ?: return
-        val searchManager = activity.getSystemService(Context.SEARCH_SERVICE) as SearchManager
-        val searchItem = menu.findItem(R.id.search_item)
-        searchView = searchItem.actionView as SearchView
-        val searchableInfo = searchManager.getSearchableInfo(requireActivity().componentName)
-        searchView!!.setSearchableInfo(searchableInfo)
-
-        val autoPlay = navArgs.autoplay
-        val query = navArgs.query
-
-        // If started with a query, enter it to the searchView
-        if (query != null) {
-            searchView!!.setQuery(query, false)
-            searchView!!.clearFocus()
-        }
-
-        searchView!!.setOnSuggestionListener(object : SearchView.OnSuggestionListener {
-            override fun onSuggestionSelect(position: Int): Boolean {
-                return true
-            }
-
-            override fun onSuggestionClick(position: Int): Boolean {
-                Timber.d("onSuggestionClick: %d", position)
-                val cursor = searchView!!.suggestionsAdapter.cursor
-                cursor.moveToPosition(position)
-
-                // 2 is the index of col containing suggestion name.
-                val suggestion = cursor.getString(2)
-                searchView!!.setQuery(suggestion, true)
-                return true
-            }
-        })
-
-        searchView!!.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String): Boolean {
-                Timber.d("onQueryTextSubmit: %s", query)
-                searchView!!.clearFocus()
-                search(query, autoPlay)
-                return true
-            }
-
-            override fun onQueryTextChange(newText: String): Boolean {
-                return true
-            }
-        })
-
-        searchView!!.setIconifiedByDefault(false)
-        searchItem.expandActionView()
     }
 
     override fun onDestroyView() {
@@ -313,7 +229,6 @@ class SearchFragment : MultiListFragment<Identifiable>(), KoinComponent {
     }
 
     private fun onAlbumSelected(album: Album, autoplay: Boolean) {
-
         val action = SearchFragmentDirections.searchToTrackCollection(
             id = album.id,
             name = album.title,

ultrasonic/src/main/kotlin/org/moire/ultrasonic/playback/PlaybackService.kt
@@ -340,6 +340,7 @@ class PlaybackService :
             // needed starting Android 12 (S = 31)
             flags = flags or FLAG_IMMUTABLE
         }
+        intent.action = Intent.ACTION_MAIN
         intent.putExtra(Constants.INTENT_SHOW_PLAYER, true)
         return PendingIntent.getActivity(this, 0, intent, flags)
     }

ultrasonic/src/main/kotlin/org/moire/ultrasonic/provider/SearchSuggestionProvider.kt
@@ -1,0 +1,24 @@
+/*
+ * SearchSuggestionProvider.kt
+ * Copyright (C) 2009-2023 Ultrasonic developers
+ *
+ * Distributed under terms of the GNU GPLv3 license.
+ */
+package org.moire.ultrasonic.provider
+
+import android.content.SearchRecentSuggestionsProvider
+import org.moire.ultrasonic.BuildConfig
+
+/**
+ * Provides search suggestions based on recent searches.
+ */
+class SearchSuggestionProvider : SearchRecentSuggestionsProvider() {
+    init {
+        setupSuggestions(AUTHORITY, MODE)
+    }
+
+    companion object {
+        val AUTHORITY = "${BuildConfig.APPLICATION_ID}.provider.SearchSuggestionProvider"
+        const val MODE = DATABASE_MODE_QUERIES
+    }
+}

ultrasonic/src/main/kotlin/org/moire/ultrasonic/provider/UltrasonicAppWidgetProvider.kt
@@ -219,8 +219,8 @@ open class UltrasonicAppWidgetProvider : AppWidgetProvider() {
                 NavigationActivity::class.java
             ).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
             if (playerActive) intent.putExtra(Constants.INTENT_SHOW_PLAYER, true)
-            intent.action = "android.intent.action.MAIN"
-            intent.addCategory("android.intent.category.LAUNCHER")
+            intent.action = Intent.ACTION_MAIN
+            intent.addCategory(Intent.CATEGORY_LAUNCHER)
             var flags = PendingIntent.FLAG_UPDATE_CURRENT
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 // needed starting Android 12 (S = 31)

commit 243653760941786d760fb6986df2b2b16e76a86d

Fix a bug introduced in 725d9281

ultrasonic/src/main/kotlin/org/moire/ultrasonic/fragment/TrackCollectionFragment.kt
@@ -36,6 +36,7 @@ import org.moire.ultrasonic.adapters.AlbumHeader
 import org.moire.ultrasonic.adapters.AlbumRowDelegate
 import org.moire.ultrasonic.adapters.HeaderViewBinder
 import org.moire.ultrasonic.adapters.TrackViewBinder
+import org.moire.ultrasonic.data.ActiveServerProvider
 import org.moire.ultrasonic.data.ActiveServerProvider.Companion.isOffline
 import org.moire.ultrasonic.domain.Identifiable
 import org.moire.ultrasonic.domain.MusicDirectory
@@ -582,7 +583,7 @@ open class TrackCollectionFragment(
             } else {
                 setTitle(name)
 
-                if (isAlbum) {
+                if (isAlbum && ActiveServerProvider.shouldUseId3Tags()) {
                     listModel.getAlbum(refresh2, id, name)
                 } else {
                     listModel.getMusicDirectory(refresh2, id, name)